df0c85ed45f8929182e9be90e0f4ada58e76fd4f
Kirill Shashichev <67040346+Qtrixnet@users.noreply.github.com>
2021-03-13 15:44:20 +0600

    feat: создать спиннер

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './spinner.css'
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-double-ring-av4fvvi9jcd">
+      <div className="ldio-0fi3zkedlx1c">
+        <div></div>
+        <div></div>
+        <div><div></div></div>
+        <div><div></div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Spinner;
